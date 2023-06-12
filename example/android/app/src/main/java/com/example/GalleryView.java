507657a7faf0e82f5ff92fe2b8f6549eb086127d
Graham Mendick <graham.mendick@gmail.com>
2023-06-12 17:52:53 +0100

    Added paintings as fragments

--- example/android/app/src/main/java/com/example/GalleryView.java
+++ example/android/app/src/main/java/com/example/GalleryView.java
@@ -1,15 +1,62 @@
 package com.example;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.facebook.react.uimanager.ThemedReactContext;
+
+import java.util.ArrayList;
+
 public class GalleryView extends ViewGroup {
+    ArrayList<View> paintings = new ArrayList<>();
+
     public GalleryView(Context context) {
         super(context);
     }
 
+    void onAfterUpdateTransaction() {
+        FragmentActivity currentActivity = (FragmentActivity) ((ThemedReactContext) getContext()).getCurrentActivity();
+        FragmentManager fragmentManager = currentActivity.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setReorderingAllowed(true);
+        PaintingFragment fragment = new PaintingFragment(paintings.get(0));
+        fragmentTransaction.replace(getId(), fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        onAfterUpdateTransaction();
+    }
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    }
 
+    public static class PaintingFragment extends Fragment {
+        private View painting;
+
+        PaintingFragment(View painting) {
+            super();
+            this.painting = painting;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return painting;
+        }
     }
 }
--- example/android/app/src/main/java/com/example/GalleryViewManager.java
+++ example/android/app/src/main/java/com/example/GalleryViewManager.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import android.view.View;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.uimanager.ThemedReactContext;
@@ -16,5 +18,31 @@ public class GalleryViewManager extends ViewGroupManager<GalleryView> {
     @Override
     protected GalleryView createViewInstance(@NonNull ThemedReactContext themedReactContext) {
         return new GalleryView(themedReactContext);
+    }
+
+    @Override
+    public void addView(GalleryView parent, View child, int index) {
+        parent.paintings.add(index, child);
+    }
+
+    @Override
+    public void removeViewAt(GalleryView parent, int index) {
+        parent.paintings.remove(index);
+    }
+
+    @Override
+    public int getChildCount(GalleryView parent) {
+        return parent.paintings.size();
+    }
+
+    @Override
+    public View getChildAt(GalleryView parent, int index) {
+        return parent.paintings.get(index);
+    }
+
+    @Override
+    protected void onAfterUpdateTransaction(@NonNull GalleryView view) {
+        super.onAfterUpdateTransaction(view);
+        view.onAfterUpdateTransaction();
     }
 }
